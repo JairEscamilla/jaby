e42aa24204018be1cb2fe3025cf1d2e3bea7e3c2
Jair Escamilla <hernandez_yo14@outlook.es>
2020-05-09 16:34:00 -0500

    Procedimiento para validar el acceso de un usuario a un album

---
+++ procedimientos/valida_entrada_album.sql
@@ -0,0 +1,28 @@
+CREATE DEFINER=`root`@`localhost` PROCEDURE `valida_entrada_album`(OUT entrada INT, usuario VARCHAR(150), album INT)
+BEGIN
+        DECLARE tAlbum, ids, salida, idAl INT;
+        DECLARE curTipoAlbum CURSOR FOR SELECT tipo FROM Album WHERE id_album = album;
+        DECLARE curValidaEntrada CURSOR FOR SELECT id_album FROM Suscripciones WHERE username = usuario;  
+        DECLARE CONTINUE HANDLER FOR NOT FOUND SET salida = 1;
+        SET salida = 0;
+        SET entrada = 0;
+        
+
+        OPEN curTipoAlbum;
+            FETCH curTipoAlbum INTO tAlbum;
+        CLOSE curTipoAlbum;
+        IF tAlbum = 1 THEN 
+            OPEN curValidaEntrada;
+                curV: REPEAT
+                    FETCH curValidaEntrada INTO ids;
+                    IF salida = 1 THEN
+                        LEAVE curV;
+                    END IF;
+                    IF ids = album THEN
+                        SET entrada = 1;
+                    END IF;
+                    UNTIL salida = 1
+                END REPEAT curV;
+            CLOSE curValidaEntrada;
+        END IF;
+    END
